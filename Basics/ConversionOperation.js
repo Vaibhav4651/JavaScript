4ef62f728146063378505745d0bb65550e536e47
Vaibhav4651 <154692391+Vaibhav4651@users.noreply.github.com>
2024-07-09 10:49:13 +0000

    Conversion

---
+++ Basics/ConversionOperation.js
@@ -0,0 +1,33 @@
+/*let score = '33abc'
+score=null
+score=undefined
+
+console.log(typeof score);
+console.log(typeof(score));
+
+
+//Converting in the number from string 
+let valueInNumber = Number(score)
+console.log(valueInNumber);//Shows NaN beacuse it also contain string vakue with number 
+console.log(typeof(valueInNumber));
+
+/*When we write null in the place of the variable decelration 
+it gives 0 as an output*/
+/*and when we use undefined it gives NaN*/
+
+/* If we give BOOLEAN values in the variable it will give the value of
+the boolean Value TRUE =1 AND FAlSE = 0*/
+
+let isLoggedIn = ""// Conversion of empty string is false
+
+isLoggedIn = "Vaibhav"//Name given in boolean conversion is True
+
+let booleanIsLoogedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoogedIn);
+
+
+let someNumber = 33
+let stringNUmber = String(someNumber)
+console.log(stringNUmber);
+console.log(typeof stringNUmber);
