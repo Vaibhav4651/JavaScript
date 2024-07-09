114c48f40c252cf199bf43bb93ecd67cac0aabbd
Vaibhav4651 <154692391+Vaibhav4651@users.noreply.github.com>
2024-07-09 00:45:41 +0000

    operations

---
+++ Basics/operations.js
@@ -0,0 +1,37 @@
+/*let value = 3
+let negValue = -value
+console.log(negValue);
+
+/*console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%3);*/
+
+/*let str1="Hello"
+let str2="Vaibhav"
+let str3=str1+str2
+console.log(str3);*/
+
+/*console.log('1'+2);
+console.log(1+"2");
+console.log('1'+'2');
+console.log("1"+2+2);*///Sara converion string ke andar ho gaya 
+
+//console.log(1+2+"2");/*o/p is 32*/
+
+//Use paranthesis for the complex calculations like
+/*(3+4*5%3)  this is not good for our code writing*/
+
+/*console.log(+true);
+
+console.log(+"");
+
+let num1,num2,num3//Not a good method to pratice and in company
+num1=num2=num3 = 2+2
+console.table([num1,num2,num3]);
+*/
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter());
