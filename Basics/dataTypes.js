1f8eeb58b505ac5c5f391b471e219b8bf9903796
Vaibhav4651 <154692391+Vaibhav4651@users.noreply.github.com>
2024-07-09 10:44:22 +0000

    dataTypes2

---
+++ Basics/dataTypes.js
@@ -0,0 +1,32 @@
+"use strict";  //treat all js code as newer version
+
+//alert(3+3)//Here is error in this beacuse we are using the node.js not browser
+
+console.log(3+3);
+
+console.log("Vaibhav")
+//Code rediability should be high
+
+let name ="Vaibhav"
+let age = 21
+let isLoggedIn = false
+
+/*Datatypes
+    number => 2 to power 53
+    bigint higher numbers 
+    String => In "..."
+    Boolean => true or false 
+    Null => standalone value 
+    Undefined => Not assigned the value 
+    symbol => Used in React for uniqueness
+*/
+
+// OBJECT => 
+    
+console.log(typeof undefined);// Undefinrd
+console.log(typeof null);//Objects
+              
+
+
+/*SpringBoard*/
+let empId = 1234
