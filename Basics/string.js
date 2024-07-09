b41a1baabde20e62cecf970ce02ecf31153f204a
Vaibhav4651 <154692391+Vaibhav4651@users.noreply.github.com>
2024-07-09 00:44:40 +0000

    string

---
+++ Basics/string.js
@@ -0,0 +1,56 @@
+/*const name ="            Vaibhav vyenkatrao shingte"
+const repoCount = 3
+console.log(name + repoCount +"value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+*/
+const gameName= new String(`vaibh av7 796`)
+
+/* Browser cha Console madhe jeva code run kartot ani mag tite Prototype madhe kahi 
+methods yetet te sagla methods apan ta sathi use karu shakto*/
+
+//Methods for the string in the browsr's Console
+
+/*console.log(gameName[0])
+console.log(gameName.__proto__)
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(5));
+console.log(gameName.indexOf('i'))
+console.log(gameName.valueOf());
+console.log(gameName.anchor());
+console.log(gameName.at(5));
+console.log(gameName.big());
+console.log(gameName.blink());
+console.log(gameName.charCodeAt(5));
+console.log(gameName.codePointAt(4));
+console.log(gameName.concat(name));
+console.log(gameName.endsWith(7796));
+console.log(gameName.fixed());
+
+console.log(gameName.fontcolor());
+console.log(gameName.includes('v'));
+
+console.log(gameName.isWellFormed());
+//console.log(gameName.lastIndexOf());
+console.log(gameName.replace('7796','JAI SHREE RAM'));
+console.log(gameName.replaceAll("vaibhav7796","GOOD MORNING JETHALAL"));
+console.log(gameName.slice(2,5));
+console.log(gameName.split("",3));
+console.log(...gameName);
+let vai = [...gameName]
+console.log(typeof(vai))
+console.log(name)
+console.log(name.trim());
+console.log(name.trimStart())
+
+console.log(name.toLocalelLowerCase)
+
+console.log(gameName.toString())
+*/
+const url = "https://vaibhav.com/vaibhav7796shingte";
+console.log(url.replace("7796","Vyenkatrao"))
+
+console.log(gameName.split(" "))
