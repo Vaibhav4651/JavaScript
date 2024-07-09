f21f644bde74ab7a5019e99bae9e2e53d362d67f
Vaibhav4651 <154692391+Vaibhav4651@users.noreply.github.com>
2024-07-09 00:43:21 +0000

    variables

---
+++ Basics/Variables.js
@@ -0,0 +1,14 @@
+const accountId = 12345
+let  accountEmail="Vaibhav@gmail.com"
+var accountPassword="123456"
+accountCity="Nanded"
+let accountState;
+//accountId=2 not allowed in JavaScript
+console.log(accountId);
+accountEmail="shingte@gmail.com"
+accountPassword="1234"
+accountCity="Hydrabad"
+console.table([accountEmail,accountId,accountCity,accountPassword,accountState])
+
+/*Don't use the var in js there is issue of scope in 
+block and functional scope*/
