9ef655055ad39f4453b384c9486a237126edfa56
Vaibhav4651 <154692391+Vaibhav4651@users.noreply.github.com>
2024-07-13 18:20:36 +0000

    function1

--- Basics2/Function.js
+++ Basics2/Function.js
@@ -9,8 +9,25 @@ function sayMyName(){
 }
 //sayMyName();
 
-function addTwoNumbers(number1,number2)
-{
-    console.log(number1+number2)
+// function addTwoNumbers(number1,number2)
+// {
+
+//     console.log(number1+number2)
+// }
+//addTwoNumbers(9,2)//Arguments
+//addTwoNumbers(5,"a")//Concate the Two numbers 5a will be the output
+//addTwoNumbers(6,null)//Giving that number which we have passed
+
+
+//After RETURN we cant define any thing and it wont execute
+function addTwoNumbers(number1,number2){
+    let result=number1+number2
+    return result
 }
-addTwoNumbers(9,2)//Arguments
+const result = addTwoNumbers(6,6)
+//console.log("Result:",result);
+
+function loginUserMesasge(){
+    return `${userName} jsut logged in`
+}
+console.log(loginUserMesasge("Vaibhav"));
