4871becdf36657293b566f57e47c8f277f66ec9b
Vaibhav4651 <154692391+Vaibhav4651@users.noreply.github.com>
2024-07-13 14:50:13 +0000

    function1

---
+++ Basics2/Function.js
@@ -0,0 +1,16 @@
+function sayMyName(){
+    console.log("V");
+    console.log("a");
+    console.log("i");
+    console.log("b");
+    console.log("h");
+    console.log("a");
+    console.log("V");
+}
+//sayMyName();
+
+function addTwoNumbers(number1,number2)
+{
+    console.log(number1+number2)
+}
+addTwoNumbers(9,2)//Arguments
