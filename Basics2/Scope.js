958f3deb31c2c632c307260aa865335ed54101d7
Vaibhav4651 <154692391+Vaibhav4651@users.noreply.github.com>
2024-07-15 17:01:26 +0000

    scope

--- Basics2/Scope.js
+++ Basics2/Scope.js
@@ -4,7 +4,7 @@ So, we mostly use LET and CONST*/
 
 //Before {..} is defined is GLOBALLY Scoped
 
-if(true){//Everything in the if is BLOCK SCOPE
+/*if(true){//Everything in the if is BLOCK SCOPE
     var c = 30
     let a=10
     const b= 20
@@ -13,4 +13,47 @@ if(true){//Everything in the if is BLOCK SCOPE
 
 //console.log(a);
 //console.log(b);
-console.log(c);
+console.log(c);*/
+
+
+//Nested Scope childcan access parent element But parent cant access chlid
+function One(){
+    const userName = "Vaibhav"
+
+    function Two(){
+        var website="Youtube"
+        console.log(userName);
+    }
+   // console.log(website);
+    Two()
+}
+One()
+
+if(true){
+    const userName="Vaibhav"
+    if(userName === "Vaibhav"){
+        const website = " YouTube"
+        console.log(userName+website);
+    }
+    //console.log(website);
+}
+
+//console.log(userName);
+
+
+// +++++++++++++++++ Interesing +++++++++++++++++
+
+function addOne(num){
+    return num+1
+}
+
+addOne(5)
+
+//Thw given function can said to be VARIABLE    
+/*HOSTING we can not call function without initializing when we decleare the 
+function by using variables*/
+const addTwo = function(num){
+    return num+2
+}
+addTwo(5)
+
