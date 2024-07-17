13b04d2772f49ce0fa6e5a6c5f30841686917b46
Vaibhav4651 <154692391+Vaibhav4651@users.noreply.github.com>
2024-07-17 17:55:38 +0000

    switch Case

---
+++ control_Flow/switch.js
@@ -0,0 +1,49 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 3
+// switch (month) {
+//     case 1:
+//         console.log("January");
+//         break;
+//     case 2:
+//         console.log("Feb");
+//         break;
+//     case 3:
+//         console.log("March");
+//         break;
+//     case 4:
+//         console.log("April");
+//         break;
+
+//     default:
+//         console.log("Please Check the input");
+//         break;
+// }
+
+
+const month = "March"
+switch (month) {
+    case 'jan':
+        console.log("January");
+        break;
+    case "feb":
+        console.log("Feb");
+        break;
+    case "March":
+        console.log("March");
+        break;
+    case "April":
+        console.log("April");
+        break;
+
+    default:
+        console.log("Please Check the input");
+        break;
+}
