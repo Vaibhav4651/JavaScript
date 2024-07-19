10d0e284f4bec5a3ee1d3be07ff938dfdf5d76ad
Vaibhav4651 <154692391+Vaibhav4651@users.noreply.github.com>
2024-07-19 17:53:17 +0000

    for_of and map

---
+++ Iterations/for_of.js
@@ -0,0 +1,23 @@
+// *********** for - of **********
+
+// //Syntax
+// for (const iterator of object) {
+    
+// }
+
+const myArr = [1,2,3,4,5]
+
+for (const Value of myArr) {
+    //console.log(Value);
+}
+
+const greetings = "Hello World!"
+
+for (const greet of greetings) {
+    //console.log(greet);
+}
+
+// Maps
+
+const newMap = new Map()
+Map.
