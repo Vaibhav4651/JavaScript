62276e04b2c71112192622d8c04f1d323ec411b0
Vaibhav4651 <154692391+Vaibhav4651@users.noreply.github.com>
2024-07-20 16:50:37 +0000

    for_of and map

--- Iterations/for_of.js
+++ Iterations/for_of.js
@@ -19,5 +19,46 @@ for (const greet of greetings) {
 
 // Maps
 
+
+/*Only unique values DUPLICATE values are not allowed in Map()
+it is not iteratable by anything*/
+
 const newMap = new Map()
-Map.
+newMap.set('IN','INDIA')
+newMap.set('US','UNITED STATES OF AMERICA')
+newMap.set('Fr','FRANCE')
+//console.log(newMap);
+
+for (const [key,Value] of newMap) {
+   // console.log([key, ':-' ,Value]);
+}
+
+
+/*For-Of loop is not iteratable by object*/
+
+// const myObj = {
+//     game1 : 'nfs',
+//     game2 : 'spiderman'
+// }
+
+// for (const [game,value] of myObj) {
+//     console.log(game , ':-' , value);
+// }
+
+const myObj ={
+    js: "JavaScript",
+    cpp: "c++",
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObj) {
+  //  console.log(`${key} Shortcut is for ${myObj[key]}`);
+}
+
+
+const programming = ['c','c++','Java','python','ruby']
+
+for (const key in programming) {
+    //console.log(programming[key]);
+}
