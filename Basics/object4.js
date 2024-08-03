b815e61128d8a783aa57fbf82528f7e72dc6e1ee
Vaibhav4651 <154692391+Vaibhav4651@users.noreply.github.com>
2024-08-03 23:10:28 +0530

    Add files via upload

---
+++ Basics/object4.js
@@ -0,0 +1,68 @@
+/*const user = new Object();
+
+const JsUser = new Object();
+
+
+//const user = new Array()
+
+user.name = "Vaibhav"
+user.age = 21
+user.dob = "2003"
+user.gender = "M"
+user.day = "Tuesday"
+user.time = "Morning"
+
+//Created the new object in the Object User
+user.details = {
+    phoneNumber: 7796801994,
+    email: "abc@gmail.com",
+    address: "Nanded",
+}
+
+
+console.log(user);
+
+console.log(Object.keys(user))
+
+//User.Detais is used to Fetch the details keys in the User Object
+console.log(Object.values(user.details));
+
+//to delete the Key in the Object We have to use the Delete Keyword
+
+delete user.age;
+
+JsUser.fullName = "Vaibhav Vyenkatrao Shingte"
+
+
+//Here we are assigning the Object with the Object
+console.log(user.JsUser = JsUser); 
+*/
+// delete JsUser.fullName
+// console.log(JsUser);
+
+// const obj1 = {
+//     name: "Vaibhav",
+//     age : 21
+// }
+
+// const obj2 = {
+//     name: "Rony",
+//     age : 27
+// }
+
+const obj1 = {
+    name: "Vaibhav",
+    age : 21
+}
+
+// const obj2 = {
+//     lang: "Rony",
+//     score : 27
+// }
+
+// //Here the values are getting Updated Beacuse the "variables in the Object are same so they are updating"
+// //If we change the variable name of the object so they will get merge properly
+// console.log(Object.assign(obj1, obj2));
+
+// const obj3 = { ...obj1, ...obj2 }
+// console.log(obj3);
